5523f07b51f521a9f17cb4c6f06d506505a7fd38
SuzukiTakumi <ne211228@senshu-u.jp>
2022-10-28 09:35:47 +0000

    “第5回授業内課題”

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :update_expiration_time
+  
   private def current_member
-    Member.find_by(id: session[:member_id]) if session[:member_id]
+    Member.find_by(id: cookies[:member_id]) if cookies[:member_id]
   end
   helper_method :current_member
 
@@ -9,5 +11,9 @@ class ApplicationController < ActionController::Base
 
   private def login_required
     raise LoginRequired unless current_member
+  end
+
+  private def update_expiration_time
+    cookies[:member_id] = {value: cookies[:member_id] , expires: 15.seconds.from_now }
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     member = Member.find_by(name: params[:name])
     if member&.authenticate(params[:password])
-      session[:member_id] = member.id
+      cookies[:member_id] = {value: member.id , expires: 15.seconds.from_now }
     else
       flash.alert = "名前とパスワードが一致しません"
     end
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session.delete(:member_id)
+    cookies.delete(:member_id)
     redirect_to :root
   end
 end
