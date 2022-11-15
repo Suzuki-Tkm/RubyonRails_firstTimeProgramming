1342e77913e435e78308b40572720924f81c4482
SuzukiTakumi <ne211228@senshu-u.jp>
2022-11-15 09:09:05 +0000

    “14.3章まで実装”

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,11 @@ Rails.application.routes.draw do
     resources :duties, only: [:index]
   end
 
+  resources :entries do
+    patch "like", "unlike", on: :member
+    get "voted", on: :collection
+  end
+
   resource :session, only: [:create, :destroy]
   resource :account, only: [:show, :edit, :update]
   resource :password, only: [:show, :edit, :update]
