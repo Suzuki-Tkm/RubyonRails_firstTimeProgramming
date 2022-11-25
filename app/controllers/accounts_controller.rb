39786174f6d5ba4a9ab4bba352c99c397fc0fbac
SuzukiTakumi <ne211228@senshu-u.jp>
2022-11-25 09:46:48 +0000

    “第8回授業内課題”

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,8 @@
 class AccountsController < ApplicationController
   layout "top"
-  before_action :login_required
+  before_action only: [:show , :edit , :update] do
+    :login_required
+  end
 
   def show
     @member = current_member
@@ -17,6 +19,19 @@ class AccountsController < ApplicationController
       redirect_to :account, notice: "アカウント情報を更新しました。"
     else
       render "edit"
+    end
+  end
+
+  def new
+    @member = Member.new(birthday: Date.new(1980 , 1 , 1))
+  end
+
+  def create
+    @member = Member.new(params[:member])
+    if @member.save
+      redirect_to :root , notice: "会員を登録しました。"
+    else
+      render "new"
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
   end
 
   resource :session, only: [:create, :destroy]
-  resource :account, only: [:show, :edit, :update]
+  resource :account, only: [:show, :edit, :update , :new , :create]
   resource :password, only: [:show, :edit, :update]
 
   resources :entries
